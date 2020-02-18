1880f8d17ac397862c7729851fdeab13fce244c0
Joseph Kitzhaber <josephkitzhaber@u.boisestate.edu>
2020-02-18 14:04:52 -0700

    added query for creating the table Purchase

--- query_set_for_concessions.sql
+++ query_set_for_concessions.sql
@@ -3,3 +3,11 @@ ItemCode varchar(10) UNIQUE,
 ItemDescription varchar(50),
 Price decimal(4,2) DEFAULT 00.00,
 primary key (ID));
+
+Create table Purchase (ID int auto_increment,
+ItemID int,
+Quantity int NOT NULL,
+PurchaseDate datetime,
+FOREIGN KEY (ItemID)
+	REFERENCES Item(ID),
+primary key (ID));
