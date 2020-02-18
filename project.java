d490588ecc2c61468e84bd105d3361ccbf6e02d9
Joseph Kitzhaber <josephkitzhaber@u.boisestate.edu>
2020-02-18 14:34:44 -0700

    added method skeletons

---
+++ project.java
@@ -0,0 +1,55 @@
+public class project{
+	
+	Class.forName("com.mysql.jdbc.Driver");
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	private CreateItem(int itemCode, String itemDescription, double price) {
+		
+	}
+	
+	private CreatePurchase(int itemCode, int purchaseQuantity) {
+		
+	}
+	
+	private CreateShipment(int itemCode, int ShipmentQuantity, String shipmentDate) {
+		
+	}
+	
+	private GetItems(int itemCode) {
+		
+	}
+	
+	private GetShipments(int itemCode) {
+		
+	}
+	
+	private GetPurchases(int itemCode) {
+		
+	}
+	
+	//MOST COMPLEX METHOD
+	private ItemsAvailable(int itemCode) {
+		
+	}
+	
+	private UpdateItem(int itemCode, double price) {
+		
+	}
+	
+	private DeleteItem(int itemCode) {
+		
+	}
+	
+	private DeleteShipment(int itemCode) {
+		
+	}
+	
+	private DeletePurchase(int itemCode) {
+		
+	}
+	
+	
+}
